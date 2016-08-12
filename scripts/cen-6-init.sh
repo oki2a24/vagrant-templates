91b367923f30f4ef41238f19fe3f441576bc43d8
oki2a24 <oki2a24@oki2a24.com>
2016-08-12 21:54:06 +0900

    コードを整理

--- scripts/cen-6-init.sh
+++ scripts/cen-6-init.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
+#
+# Usage:
+#   cen-6-init.sh
+#
+# Description:
+#   CentOS 6 の基本的なインストール・設定を行います。
+#   不要なデーモンのストップ
+#   ファイアウォールを全受け入れ設定
+#   既存パッケージのアップデート
+#   Vim インストール
+#   NTP インストール、設定、起動
+#   SELINUX を無効化"
+#
+###########################################################################
 
-set -e
-set -x
+set -eux
 
-yum update -y
-
-# Vim
-yum install -y vim-enhanced
-
-# 時刻同期
-# NTP サーバーインストール、設定、起動
-yum install -y ntp
-chkconfig ntpd on
-/etc/init.d/ntpd start
-# selinuxを無効
-sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
-setenforce permissive
-# メモリ節約。不要なデーモンをストップ
+echo "メモリ節約。不要なデーモンをストップ"
+set +e
 chkconfig auditd off
 chkconfig lvm2-monitor off
 chkconfig mdmonitor off
 chkconfig netfs off
 chkconfig restorecond off
-# ファイアウォール
+chkconfig udev-post off
+set -e
+echo "ファイアウォール。すべて受入"
 iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -F
 service iptables save
+
+yum update -y
+
+echo "Vim インストール"
+yum install -y vim-enhanced
+
+echo "時刻同期。NTP サーバーインストール、設定、起動"
+yum install -y ntp
+chkconfig ntpd on
+/etc/init.d/ntpd start
+
+echo "SELINUX を無効化"
+sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+setenforce permissive
